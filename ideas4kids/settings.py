6f86ede14b9d1040b8afaead300b8745cbebec51
Richard Boulton <richard@tartarus.org>
2017-11-22 08:40:02 +0000

    Add Sentry configuration

--- ideas4kids/settings.py
+++ ideas4kids/settings.py
@@ -177,3 +177,15 @@ MEDIA_URL = '/media/'
 ADMIN_MEDIA_PREFIX = '/adminstatic/'
 
 '''
+
+
+# Sentry configuration
+SENTRY_DSN = os.environ.get('SENTRY_DSN')
+if SENTRY_DSN:
+    INSTALLED_APPS += (
+        'raven.contrib.django.raven_compat',
+    )
+    RAVEN_CONFIG = {
+        'dsn': SENTRY_DSN,
+        'release': os.environ.get('HEROKU_SLUG_COMMIT', ''),
+    }
