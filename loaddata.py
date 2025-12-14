b0ef464a7fd46a41a90683ac538fc0c73beb1996
Richard Boulton <boulton.rj@gmail.com>
2025-12-14 20:43:12 +0000

    Fix for python3

--- loaddata.py
+++ loaddata.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from django.conf import settings
 from django.core import management
@@ -7,7 +7,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    print 'Type "yes" to replace database contents with archive contents'
+    print('Type "yes" to replace database contents with archive contents')
     val = sys.stdin.readline().strip().lower()
     if val == 'yes':
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ideas4kids.settings")
